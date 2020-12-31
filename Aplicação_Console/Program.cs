34e73c5ceb54a78259cdb2e28fe5bb68c6334da6
unknown <adilson100-haker@hotmail.com>
2020-12-31 14:07:33 -0300

    Progama simples, apenas teste

---
+++ Aplicação_Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aplicação_Console
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            for(int i = 0; i <=4; i ++){
+                Console.WriteLine( $"Apenas um teste número: {i}");
+            }
+        }
+    }
+}
