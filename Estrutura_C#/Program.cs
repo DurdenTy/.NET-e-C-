59d372ba2d2d832a4c336cf8c61e694f1a6f0466
unknown <adilson100-haker@hotmail.com>
2020-12-31 17:22:58 -0300

    nada

---
+++ Estrutura_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Estrutura_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome = "adilson";
+            Console.WriteLine($"Hello World, i am {nome}");
+        }
+    }
+}
