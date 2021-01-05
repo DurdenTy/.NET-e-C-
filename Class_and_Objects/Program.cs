a5e7963795421660adaf8a1ff2698392874638c1
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-01-05 13:17:56 -0300

    Update Program.cs

--- Class_and_Objects/Program.cs
+++ Class_and_Objects/Program.cs
@@ -43,37 +43,6 @@ namespace Class_and_Objects
 
             Ponto3D Cubo = new Ponto3D(7, 7, 7);
 
-
-            Console.WriteLine("\t\tMenu interativo\n");
-            Console.WriteLine("Opção 1 - Adicionar arquivo");
-            Console.WriteLine("Opção 2 - Listar arquivos");
-            Console.WriteLine("Opção 3 - Somar quantidade de arquivos");
-            Console.WriteLine("Opção Q - Sair\n\n");
-
-            string opcao = Console.ReadLine();
-
-            while(opcao.ToUpper() != "Q"){
-
-                switch(opcao){
-
-                    case "1":
-                        //Adicionar
-                        break;
-
-                    case "2":
-                        //Listar
-                        break;
-
-                    case "3":
-                        //Somar quantidade
-                        break;
-
-                    default:
-
-                        throw new Exception("Não está entre 1 e 3, o programa irá ser fechado");
-
-                }
-            }  
         }
     }
 }
