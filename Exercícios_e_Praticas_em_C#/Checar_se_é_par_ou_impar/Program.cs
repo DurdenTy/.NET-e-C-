81d37de23c320dd7e11b80e59a3ac7dfc74337dd
Adilson de Lima nascimento <60784414+DurdenTy@users.noreply.github.com>
2021-01-06 15:10:30 -0300

    Add files via upload

---
+++ Exercícios_e_Praticas_em_C#/Checar_se_é_par_ou_impar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Checar_se_é_par_ou_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Saber se o seu número é par ou impar\n\n");
+            Console.WriteLine("Coloque o seu número");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if(num == 0){
+                Console.WriteLine("Seu número é neutro");
+            }
+            else if(num % 2 == 0){
+                Console.WriteLine("Seu número é par");
+            }else{
+                Console.WriteLine("Seu número não é par");
+            }
+        }
+    }
+}
---
+++ Exercícios_e_Praticas_em_C#/Somar_Média_entre_5_variáveis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Somar_Média_entre_5_variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double item, item2, item3, item4, item5, media;
+
+            Console.WriteLine("Calcular a média entre 5 variáveis\n\n");
+
+            Console.WriteLine("Coloque o var 1: ");
+            item = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Coloque o var 2: ");
+            item2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Coloque o var 3: ");
+            item3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Coloque o var 4: ");
+            item4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Coloque o var 5: ");
+            item5 = Convert.ToDouble(Console.ReadLine());
+
+            media = (item + item2 + item3 + item4 + item5)/5;
+
+            Console.WriteLine("A média é igual a:" + media);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
